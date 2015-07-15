67103c3020b4007c5369c55dac40e271c00249a9
Dav Clark <davclark@gmail.com>
2015-07-14 23:37:45 -0700

    All the fixins to make forms

---
+++ html2pdf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from sys import argv, exit
+from os import path
+
+from PyQt4.QtCore import QObject, SIGNAL, QUrl
+from PyQt4.QtGui import QApplication, QPrinter
+# Note: QTextDocument only supports a subset of HTML
+from PyQt4.QtWebKit import QWebView
+
+app = QApplication(argv[:-2])
+
+web = QWebView()
+# with open(argv[1]) as html_file:
+#     html_txt = html_file.read()
+# web.setHtml(html_txt)
+full_path = path.abspath(argv[1])
+web.setUrl(QUrl.fromLocalFile(full_path))
+# web.show()
+
+printer = QPrinter()
+printer.setPageSize(QPrinter.Letter)
+printer.setOutputFormat(QPrinter.PdfFormat)
+printer.setOutputFileName("file.pdf")
+
+def convertIt():
+    web.print_(printer)
+    print("Pdf generated")
+    QApplication.exit()
+
+# .setHtml is synchronous (I think)
+QObject.connect(web, SIGNAL("loadFinished(bool)"), convertIt)
+
+exit(app.exec_())
