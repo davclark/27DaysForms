139d33e70f5553f053eddfa0f4766371c4523427
Dav Clark <davclark@berkeley.edu>
2015-10-24 19:10:03 -0700

    Updated to Qt5, which seems nice!

--- html2pdf.py
+++ html2pdf.py
@@ -3,19 +3,26 @@
 from sys import argv, exit
 from os import path
 
-from PyQt4.QtCore import QObject, SIGNAL, QUrl
-from PyQt4.QtGui import QApplication, QPrinter
+from PyQt5.QtCore import QUrl
+# It'd be nice to just use QCoreApplication, but it appears that the QWebView
+# needs to be in a GUI app
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtPrintSupport import QPrinter
 # Note: QTextDocument only supports a subset of HTML
-from PyQt4.QtWebKit import QWebView
+from PyQt5.QtWebKitWidgets import QWebView
 
 app = QApplication(argv[:-2])
 
 web = QWebView()
+# This didn't work:
 # with open(argv[1]) as html_file:
 #     html_txt = html_file.read()
 # web.setHtml(html_txt)
+
 full_path = path.abspath(argv[1])
 web.setUrl(QUrl.fromLocalFile(full_path))
+
+# If you want to actually see these things
 # web.show()
 
 printer = QPrinter()
@@ -25,12 +32,13 @@ basename, _ = path.splitext(path.basename(argv[1]))
 print(basename)
 printer.setOutputFileName('pdf_forms/' + basename + '.pdf')
 
+
 def convertIt():
-    web.print_(printer)
+    web.print(printer)
     print("Pdf generated")
     QApplication.exit()
 
-# .setHtml is synchronous (I think)
-QObject.connect(web, SIGNAL("loadFinished(bool)"), convertIt)
+# May not be necessary with .setHtml, which is synchronous (I think)
+web.loadFinished.connect(convertIt)
 
-exit(app.exec_())
+exit(app.exec())
