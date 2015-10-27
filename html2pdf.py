15449af7988dd64dab50dc58a27b16a5ff14edca
Dav Clark <davclark@berkeley.edu>
2015-10-26 21:00:06 -0700

    Much ado about nothing? But all working

--- html2pdf.py
+++ html2pdf.py
@@ -1,44 +1,57 @@
-#!/usr/bin/env python
-
-from sys import argv, exit
-from os import path
+#!/usr/bin/env python3
 
 from PyQt5.QtCore import QUrl
-# It'd be nice to just use QCoreApplication, but it appears that the QWebView
+
+# It'd be nice to just use QCoreApplication, but the QWebPage
 # needs to be in a GUI app
+# We can probably run headless like so (commands are for debian-style distro)
+# sudo apt-get install xvfb
+# xvfb-run python render.py
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtPrintSupport import QPrinter
-# Note: QTextDocument only supports a subset of HTML
-from PyQt5.QtWebKitWidgets import QWebView
 
-app = QApplication(argv[:-2])
-
-web = QWebView()
-# This didn't work:
-# with open(argv[1]) as html_file:
-#     html_txt = html_file.read()
-# web.setHtml(html_txt)
-
-full_path = path.abspath(argv[1])
-web.setUrl(QUrl.fromLocalFile(full_path))
-
-# If you want to actually see these things
-# web.show()
-
-printer = QPrinter()
-printer.setPageSize(QPrinter.Letter)
-printer.setOutputFormat(QPrinter.PdfFormat)
-basename, _ = path.splitext(path.basename(argv[1]))
-print(basename)
-printer.setOutputFileName('pdf_forms/' + basename + '.pdf')
+# This is an older implementation of the Qt web stack The more recent stack is
+# the Qt WebEngine, but it's not as tightly packaged and apparently still
+# doesn't compile on Windows:
+# http://pyqt.sourceforge.net/Docs/PyQt5/introduction.html#module-PyQt5.QtWebEngineWidgets
+# For now, this is probably best if it's working (it's deprecated...)
+from PyQt5.QtWebKitWidgets import QWebPage
 
 
-def convertIt():
-    web.print(printer)
-    print("Pdf generated")
-    QApplication.exit()
+class PrintHTML(QWebPage):
 
-# May not be necessary with .setHtml, which is synchronous (I think)
-web.loadFinished.connect(convertIt)
+    # Note sure if parent is necessary - got it from
+    # https://gist.github.com/gciotta/7766803
+    def __init__(self, infile, outfile):
+        super(PrintHTML, self).__init__()
+        url = QUrl.fromLocalFile(infile)
+        self.mainFrame().setUrl(url)
 
-exit(app.exec())
+        # If you want to actually see these things
+        # self.show()
+
+        self.printer = QPrinter()
+        self.printer.setOutputFileName(outfile)
+        self.printer.setOutputFormat(QPrinter.PdfFormat)
+        self.printer.setPaperSize(QPrinter.Letter)
+
+        self.loadFinished.connect(self.convertIt)
+
+    def convertIt(self):
+        self.mainFrame().print(self.printer)
+        print('printed:', self.printer.outputFileName())
+        QApplication.exit()
+
+
+if __name__ == '__main__':
+    from sys import argv, exit
+    from os import path
+
+    in_path = path.abspath(argv[1])
+    basename, _ = path.splitext(path.basename(argv[1]))
+    outfile = 'pdf_forms/' + basename + '.pdf'
+
+    app = QApplication(argv[:-2])
+    ph = PrintHTML(in_path, outfile)
+    exit(app.exec())
+
